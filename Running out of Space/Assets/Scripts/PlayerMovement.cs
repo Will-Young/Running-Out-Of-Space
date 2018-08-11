cd820c4f3bc41acb00fae6cc6117309df1021250
Will-Young <will.c.young@hotmail.com>
2018-08-11 12:45:13 +1000

    Player Movement
    Added in player movement using Unity's physics engine

---
+++ Running out of Space/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    Rigidbody2D rb;
+
+    public float speed = 7.5f;
+
+    float moveHorizontal;
+    float moveVertical;
+
+    Vector2 movement;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Movement();
+	}
+
+    void Movement()
+    {
+        moveHorizontal = Input.GetAxis("Horizontal");
+        moveVertical = Input.GetAxis("Vertical");
+
+        movement = new Vector2(moveHorizontal, moveVertical);
+
+        rb.velocity = movement * speed;
+    }
+}
